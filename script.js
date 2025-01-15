85b4bd624f1f182931463ae11fa0b4c5ec96aca7
borvux <bennyjoram@gmail.com>
2025-01-15 12:22:46 -0600

    created a flip mail invite, css to animate it

---
+++ script.js
@@ -0,0 +1,26 @@
+const cover = document.querySelector('.cover');
+const openContent = document.querySelector('.open-content');
+const closedContent = document.querySelector('.closed-content');
+const checkbox = document.querySelector('#checkbox-cover');
+
+openContent.style.display = 'none';
+
+cover.addEventListener('transitionend', (event) => {
+  if (event.propertyName === 'transform') {
+    if (checkbox.checked) {
+      openContent.style.display = 'flex';
+    } else {
+      openContent.style.display = 'none';
+      closedContent.style.visibility = 'visible';
+    }
+  }
+});
+
+checkbox.addEventListener('change', () => {
+  if (checkbox.checked) {
+    closedContent.style.visibility = 'hidden';
+    openContent.style.display = 'none';
+  } else {
+    openContent.style.display = 'none';
+  }
+});
